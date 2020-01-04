f7c50b5a8603b4e912466e436d842b325739007b
rick <rick@ricklopez.io>
2020-01-04 12:06:33 -0500

    initial commit

---
+++ 001/index.js
@@ -0,0 +1,36 @@
+
+//Get the H1 element in the BluePrint / DOM
+
+var greetingElement = document.getElementById("greeting");
+console.log(greetingElement);
+
+// Change the text
+
+greetingElement.innerHTML = "Hello, mundo!"
+
+
+
+/*
+*
+* Data - variables [strings, numbers, objects, arrays, function]
+*
+*/
+var name = "value";
+
+/*
+* Actions => nameOfAction() - functions
+*
+*/
+console.log("called the log action");
+
+
+
+/*
+*
+* Objects - Almost everything in JS is an Object
+* Group related DATA and ACTIONS
+*/
+
+
+
+
---
+++ 002/index.js
@@ -0,0 +1,29 @@
+// Data - variables
+
+var agnes = "Agnes"; //String, numbers, bool
+var agnesAge = 10;
+
+var monaLisa = "Mona Lisa";
+var monaLisaAge = 15;
+
+// Actions - functions, operators
+
+console.log(agnes);
+
+function bark(pet){
+  
+  // Type Checking String
+  console.log("Woof, Woof - ",  pet);
+}
+
+bark(agnes);
+bark(monaLisa);
+bark("Athena");
+bark(10);
+bark(agnesAge);
+
+
+// Objects
+
+
+
