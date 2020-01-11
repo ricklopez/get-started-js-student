3a2121a2cdbd3fb18b89782de96310ef5a5498da
rick <rick@ricklopez.io>
2020-01-11 12:57:38 -0500

    003

---
+++ 003/index.js
@@ -0,0 +1,22 @@
+console.log("Hello from JS");
+var ele = document.getElementsByTagName("H1");
+ele[0].innerHTML = 'Hello from JS';
+
+
+// Data
+var pet = "Agnes"; //String, Numbers, bool, object, function, array
+
+
+// Actions
+function logAnimal(animal){
+  
+  console.log("Animal:", animal);
+  
+}
+
+logAnimal(pet);
+
+
+
+// Objects
+// Collection of related Data and Actions
